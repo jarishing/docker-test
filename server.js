f0de94e9f5f2e07114d960c315edde2c3f8fc00a
jarishing <jarishing@gmail.com>
2021-02-23 10:04:10 +0800

    20210223

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 'use strict';
 
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
+const helmet = require("helmet");
+// const fs = require('fs');
+// const https = require('https');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const credentials = {
-  key: fs.readFileSync(__dirname + '/ssl/key.pem'),
-  cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
-};
+// const credentials = {
+//   key: fs.readFileSync(__dirname + '/ssl/key.pem'),
+//   cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
+// };
 
 
 // App
 const app = express();
+app.use(helmet());
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 //https
-var httpsServer = https.createServer(credentials, app);
+// var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(PORT, () => {
-  console.log("Https server listing on port : " + PORT)
-});
+// httpsServer.listen(PORT, () => {
+//   console.log("Https server listing on port : " + PORT)
+// });
 
 
-// app.listen(PORT, HOST);
-// console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
